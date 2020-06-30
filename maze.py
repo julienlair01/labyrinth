5f3b247eb7d361b85beb6473bd6a487a2e5dc41f
Julien Lair <julien.lair@gmail.com>
2020-06-30 21:13:05 +0200

    fixed some PEP8 issues

--- maze.py
+++ maze.py
@@ -8,33 +8,26 @@ import tile
 import player
 import element
 
-# Initialize program
-pygame.init()
-# Assign FPS a value
 FPS = 30
-FramePerSec = pygame.time.Clock()
 TILESIZE = 50
 
+pygame.init()
+FramePerSec = pygame.time.Clock()
 level = level.Level()
 player = player.Player(level)
-
-pygame.display.set_caption("Escape MacGyver") 
+pygame.display.set_caption("Escape MacGyver")
 foundExit = False
 
 while not player.has_found_exit(level):
-    
-    DISPLAYSURF = pygame.display.set_mode((50 * level.width, 50 * level.height))
+    DISPLAYSURF = pygame.display.set_mode((50 * level.width,
+                                           50 * level.height)) # not sure of the indent for the 2nd line
     level.draw(DISPLAYSURF)
-    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-      
     player.update(level, 50 * level.width, 50 * level.height)
     player.draw(DISPLAYSURF)
-
     if player.has_found_exit(level):
         print("Congrats, you escaped!")
-
     FramePerSec.tick(FPS)
