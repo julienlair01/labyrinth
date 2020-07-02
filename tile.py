ddb978b8c5cbf6e42e30a8a39011d4d9d72cefb8
Julien Lair <julien.lair@gmail.com>
2020-07-02 18:33:19 +0200

    x changed in pos_x, y in pos_y

--- tile.py
+++ tile.py
@@ -12,26 +12,26 @@ class Tile():
 
     def __init__(self, tile_type, image, x, y, is_blocking):
         self.tile_type = tile_type
-        self.x = x
-        self.y = y
+        self.pos_x = x
+        self.pos_y = y
         self.is_blocking = is_blocking
         absolute_path = os.path.join(os.path.dirname(__file__), image)
         self.image = pygame.image.load(absolute_path)
         if self.tile_type == "exit":
             self.add_element("guard")
         self.surf = pygame.Surface((TILESIZE, TILESIZE))
-        self.rect = self.surf.get_rect(topleft=(TILESIZE * self.x, TILESIZE * self.y))
+        self.rect = self.surf.get_rect(topleft=(TILESIZE * self.pos_x, TILESIZE * self.pos_y))
 
     def add_element(self, element_type):
         """Adds an element on the tile (guard or item)"""
         if element_type == "guard":
-            self.element = element.Element(self.x, self.y, element_type, "assets/Gardien.png")
+            self.element = element.Element(self.pos_x, self.pos_y, element_type, "assets/Gardien.png")
         elif element_type == "plastic_tube":
-            self.element = element.Element(self.x, self.y, element_type, "assets/plastic_tube.png")
+            self.element = element.Element(self.pos_x, self.pos_y, element_type, "assets/plastic_tube.png")
         elif element_type == "needle":
-            self.element = element.Element(self.x, self.y, element_type, "assets/needle.png")
+            self.element = element.Element(self.pos_x, self.pos_y, element_type, "assets/needle.png")
         elif element_type == "ether":
-            self.element = element.Element(self.x, self.y, element_type, "assets/ether.png")
+            self.element = element.Element(self.pos_x, self.pos_y, element_type, "assets/ether.png")
 
     def get_info(self):
         return {"tile_type": self.tile_type, "is_blocking": self.is_blocking}
