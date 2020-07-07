40c91db9b965942586b3a008ec692c1a131b5821
Julien Lair <julien.lair@gmail.com>
2020-07-07 18:20:23 +0200

    Docstrings for ui.py

--- ui.py
+++ ui.py
@@ -12,11 +12,9 @@ import level
 
 class UI():
 
-    # init surface and font and display tick
-    # draw and update objects and texts
-    # manage user inputs
 
     def __init__(self):
+        """ Constructor of the calass UI. It initializes pygame. """
         pygame.init()
         pygame.font.init()
         pygame.display.set_caption("Escape MacGyver")
@@ -25,13 +23,15 @@ class UI():
         self.frame_per_sec = pygame.time.Clock()
 
     def draw_ui(self, level, player):
+        """ Draws the entire game UI by calling the respective method
+        for each element of the ui """
         self.draw_level(level)
         self.draw_player(player)
         self.draw_text()
 
     def draw_level(self, level):
-        """Draws the map by drawing each tile of the grid,
-        and the element located on the tile"""
+        """ Draws the map by drawing each tile of the grid,
+        and the element located on the tile """
         self.displaysurf = pygame.display.set_mode((TILESIZE * level.width, TILESIZE * level.height + TILESIZE))
         for pos_y in range(0, level.height):
             for pos_x in range(0, level.width):
@@ -43,10 +43,15 @@ class UI():
                     continue
 
     def draw_element(self, element):
+        """ Draws an element on a specific tile:
+        can be one of the unpicked items or the guard """
         if not element.is_picked:
             self.displaysurf.blit(element.image, element.rect)
 
     def draw_player(self, player):
+        """ Draws the player on the grid, according to its position.
+        Draws also the player's bag, containing the items picked
+        by the player. """
         self.displaysurf.blit(player.image, player.rect)
         if player.bag:
             for index, value in enumerate(player.bag):
@@ -58,9 +63,15 @@ class UI():
                 self.displaysurf.blit(image, rect)
 
     def draw_text(self):
+        """ Draws the text representing the plyaer's bag
+        at the bottom of the screen. """
         self.displaysurf.blit(self.text, (45, 765))
 
     def update_player(self, level, player):
+        """ Converts the user input into a moving direction
+        for the player. It only updates the sprite of the player
+        and calls the player method move, which manages
+        the coordinates update. """
         pressed_keys = pygame.key.get_pressed()
         if player.rect.top > 0 and pressed_keys[K_UP] and level.can_move(player.pos_x, player.pos_y - 1):
             player.rect.move_ip(0, -TILESIZE)
