77b385da674e995ace6b0c8fc1e3ed5ef494ddc6
Julien Lair <julien.lair@gmail.com>
2020-07-07 20:42:38 +0200

    changed the way the main loop behaves

--- maze.py
+++ maze.py
@@ -12,7 +12,7 @@ level = level.Level()
 player = player.Player(level)
 ui = ui.UI()
 end_of_game = False
-# game_mode = input("Do you want to play the text or the nice UI version (type ui or text)\n")
+# game_mode = input("Do you want to play the text or the nice UI version (type text or ui to continue)\n")
 game_mode = "ui"
 
 while not end_of_game:
@@ -20,16 +20,16 @@ while not end_of_game:
         ui.draw_ui(level, player)
         ui.pygame_event_get()
         ui.update_player(level, player)
+        player.pick_item(level)
+        ui.frame_per_sec.tick(FPS) # not satisfied of having it here
     elif game_mode == "text":
         # text.update.player(level, player)
+        # player.pick_item(level)
         print("Not supported yet... coming soon...")
         end_of_game = True
-        pass
-    player.pick_item(level)
     if player.has_found_exit(level):
         end_of_game = True
         if player.has_picked_all_items():
             print("Congratulationss, you escaped!")
         else:
             print("Ooops... You did not pick all required items... You lost! HAHAHAHA!!!" )
-    ui.frame_per_sec.tick(FPS)
